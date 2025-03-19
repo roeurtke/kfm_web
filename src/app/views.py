b5e377e4cf73fa7997072375f3a8c05ee3b9e501
roeurtke <kesey.roeurth@gmail.com>
2025-03-20 01:21:20 +0700

    Added template source code.

--- src/app/views.py
+++ src/app/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    return render(request, 'dashboard.html')
+
+def login_view(request):
+    return render(request, 'login.html')
+
+def register(request):
+    return render(request, 'register.html')
