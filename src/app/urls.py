9c7b4b651535db3a26f404c5606631528c1301ea
roeurtke <kesey.roeurth@gmail.com>
2025-03-22 02:26:48 +0700

    Updated src/app/urls.py file.

--- src/app/urls.py
+++ src/app/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('dashboard/', views.index, name='dashboard'),
+    path('', views.login_view, name='login'),
     path('login/', views.login_view, name='login'),
+    path('dashboard/', views.index, name='dashboard'),
     path('register/', views.register, name='register'),
 ]
