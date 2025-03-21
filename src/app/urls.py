3339b88fce2d871a6918e76d9d28cf2fc090eaa3
kroeurt <kesei.roeurt@allweb.com.kh>
2025-03-21 17:53:03 +0700

    Updated login.html,settings.py files.

--- src/app/urls.py
+++ src/app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='dashboard'),
+    path('dashboard/', views.index, name='dashboard'),
     path('login/', views.login_view, name='login'),
     path('register/', views.register, name='register'),
 ]
--- src/src/settings.py
+++ src/src/settings.py
@@ -38,11 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'app',
-    'corsheaders',
 ]
 
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -54,9 +52,19 @@ MIDDLEWARE = [
 
 CORS_ALLOW_ALL_ORIGINS = True
 
-CORS_ALLOWED_ORIGINS = [
-    "http://127.0.0.1:9000",  # Your frontend URL
-    "http://localhost:9000",
+CORS_ALLOW_METHODS = [
+    "GET",
+    "POST",
+    "PUT",
+    "PATCH",
+    "DELETE",
+    "OPTIONS"  # Add this!
+]
+CORS_ALLOW_HEADERS = [
+    "authorization",
+    "content-type",
+    "x-csrftoken",
+    "x-requested-with",
 ]
 
 ROOT_URLCONF = 'src.urls'
