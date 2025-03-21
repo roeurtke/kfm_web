7c00ffc94b7dd63eefa0c20da49b2e38f12aa399
kroeurt <kesei.roeurt@allweb.com.kh>
2025-03-21 16:21:20 +0700

    Updated requirement.txt,login.html,and settings.py files.

--- src/src/settings.py
+++ src/src/settings.py
@@ -38,9 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'app',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -48,6 +50,13 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOW_ALL_ORIGINS = True
+
+CORS_ALLOWED_ORIGINS = [
+    "http://127.0.0.1:9000",  # Your frontend URL
+    "http://localhost:9000",
 ]
 
 ROOT_URLCONF = 'src.urls'
